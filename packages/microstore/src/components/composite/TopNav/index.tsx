b976d8cf54823ff7c8ab6e7c55e71ae1362362df
helua <helua@users.noreply.github.com>
2023-11-30 14:26:46 +0100

    no https customDomain

--- packages/microstore/src/components/composite/TopNav/index.tsx
+++ packages/microstore/src/components/composite/TopNav/index.tsx
@@ -24,7 +24,7 @@ export const TopNav: FC<Props> = ({ logoUrl, companyName, showCartIcon }) => {
           {showCartIcon ? (
             <CartLinkStyled
               data-test-id="link-view-cart"
-              customDomain="https://cart.athletic-house.pl"
+              customDomain="cart.athletic-house.pl"
               type="mini"
               label={
                 <>
