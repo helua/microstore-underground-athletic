5ba68a3238d52cbccedde963a95eebdc241396e0
Helua <helua@MacBook-Air-Helua.local>
2024-07-05 23:28:18 +0200

    itemsLimit and pageSize changed in getSkusWithQuantity

--- packages/microstore/src/providers/SkuListProvider/getSkusWithQuantity.ts
+++ packages/microstore/src/providers/SkuListProvider/getSkusWithQuantity.ts
@@ -40,14 +40,14 @@ const skuFields = {
 export async function getSkusWithQuantity({
   skuList,
   cl,
-  itemsLimit = 12,
+  itemsLimit = 100,
 }: {
   skuList: SkuList
   cl: CommerceLayerClient
   itemsLimit?: number
 }): Promise<SkuWithQuantity[]> {
   const pageSize = (
-    itemsLimit <= 12 ? itemsLimit : 12
+    itemsLimit <= 100 ? itemsLimit : 100
   ) as QueryParamsList["pageSize"]
   if (skuList.manual === true) {
     const items = await cl.sku_lists.sku_list_items(skuList.id, {
