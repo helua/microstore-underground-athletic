b7857325b9d4615bda043b178a998c4b3c0a4ed1
Helua <helua@MacBook-Air-Helua.local>
2024-07-06 00:37:38 +0200

    14

--- packages/microstore/src/providers/SkuListProvider/getSkusWithQuantity.ts
+++ packages/microstore/src/providers/SkuListProvider/getSkusWithQuantity.ts
@@ -40,14 +40,14 @@ const skuFields = {
 export async function getSkusWithQuantity({
   skuList,
   cl,
-  itemsLimit = 12,
+  itemsLimit = 14,
 }: {
   skuList: SkuList
   cl: CommerceLayerClient
   itemsLimit?: number
 }): Promise<SkuWithQuantity[]> {
   const pageSize = (
-    itemsLimit <= 12 ? itemsLimit : 12
+    itemsLimit <= 14 ? itemsLimit : 14
   ) as QueryParamsList["pageSize"]
   if (skuList.manual === true) {
     const items = await cl.sku_lists.sku_list_items(skuList.id, {
